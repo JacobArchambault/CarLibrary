4728c220ebf691e57269a1d6e1464ccc9a93795a
Jacob Archambault <jacob.archambault@waystar.com>
2020-01-13 16:19:18 -0500

    Add MusicMedia Enum, MessageBox, and TurnOnRadio method to Car class.

--- Car.cs
+++ Car.cs
@@ -1,9 +1,20 @@
-﻿namespace CarLibrary
+﻿using System.Windows.Forms;
+
+namespace CarLibrary
 {
     // Represents the state of the engine.
     public enum EngineState { engineAlive, engineDead }
 
-    // The abstract base class in the hierarchy
+    // Which type of music player does this car have?
+    public enum MusicMedia
+    {
+        musicCd,
+        musicTape,
+        musicRadio,
+        musicMp3
+    }
+
+    // The abstract base class in the hierarchy.
     public abstract class Car
     {
         public string PetName { get; set; }
@@ -14,10 +25,16 @@
         public EngineState EngineState => egnState;
         public abstract void TurboBoost();
 
-        public Car() { }
+        public Car() => MessageBox.Show("CarLibrary Version 2.0!");
         public Car(string name, int maxSp, int currSp)
         {
+            MessageBox.Show("CarLibrary Version 2.0!");
             PetName = name; MaxSpeed = maxSp; CurrentSpeed = currSp;
         }
+
+
+        public void TurnOnRadio(bool musicOn, MusicMedia mm)
+            => MessageBox.Show(musicOn ? $"Jamming {mm}" : "Quiet time...");
+
     }
 }
